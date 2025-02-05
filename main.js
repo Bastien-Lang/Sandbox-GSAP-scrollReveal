86dfb77032227efd300c03b4c15c65dcec2bc96d
Markhus <hounsoumarkhus2004@gmail.com>
2025-02-05 14:16:19 +0100

    Refactor script imports in index.html and main.js for improved path management

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { gsap } from "./node_modules/gsap/index.js";
+import { gsap } from "gsap";
 import { Flip } from "gsap/Flip";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Observer } from "gsap/Observer";
